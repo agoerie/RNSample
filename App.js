c626e6808d65a0f45dc4c166d31c409330de4988
Agung Wijaya <proyekagung@gmail.com>
2022-10-11 19:28:37 +0700

    Create flatlist with infinite scroll

--- App.js
+++ App.js
@@ -6,92 +6,121 @@
  * @flow strict-local
  */
 
-import React from 'react';
-import type {Node} from 'react';
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
+
 import {
-  SafeAreaView,
-  ScrollView,
   StatusBar,
   StyleSheet,
   Text,
   useColorScheme,
   View,
+  FlatList,
+  Image,
 } from 'react-native';
-
-import {Colors, Header} from 'react-native/Libraries/NewAppScreen';
 
 import Config from 'react-native-config';
 
-const Section = ({children, title}): Node => {
+const App = () => {
+  const [nextUrl, setNextUrl] = useState(
+    'https://dev-dummy-api.jelantah.org/api/foods/get',
+  );
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(false);
+
   const isDarkMode = useColorScheme() === 'dark';
+
+  useEffect(() => {
+    fetchData();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  const getData = () => {
+    axios.get(`${nextUrl}`).then(res => {
+      setNextUrl(res?.data?.data?.next_page_url);
+      setData([...data, ...res?.data?.data?.data]);
+    });
+  };
+
+  const fetchData = () => {
+    setLoading(true);
+
+    axios
+      .get('https://dev-dummy-api.jelantah.org/api/foods/get')
+      .then(res => {
+        setNextUrl(res?.data?.data?.next_page_url);
+        setData(res?.data?.data?.data);
+      })
+      .catch(err => console.error('An error occurred', err))
+      .finally(() => setLoading(false));
+  };
+
+  const renderItem = ({index, item}) => {
+    return (
+      <View style={styles.containerImage}>
+        <Image
+          key={0}
+          style={styles.images}
+          source={{
+            uri: item?.url_image_absolute,
+          }}
+          resizeMode={'cover'}
+        />
+        <Text>{item?.food_name}</Text>
+      </View>
+    );
+  };
+
   return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
+    <View style={styles.wrapper}>
+      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
+      <View style={styles.header} />
+      <FlatList
+        style={styles.flatList}
+        contentContainerStyle={styles.containerList}
+        showsVerticalScrollIndicator={false}
+        vertical
+        data={data}
+        renderItem={renderItem}
+        keyExtractor={(item, index) => index.toString()}
+        refreshing={loading}
+        onRefresh={fetchData}
+        onEndReachedThreshold={0.2}
+        onEndReached={({distanceFromEnd}) => {
+          getData();
+        }}
+      />
     </View>
   );
 };
 
-const App: () => Node = () => {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title={''}>
-            Environment:{' '}
-            {Config.ISDEV === 'true' ? 'Development' : 'Production'}
-          </Section>
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
-};
-
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  wrapper: {
+    flex: 1,
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
+  header: {
+    backgroundColor: '#cccc',
+    width: '100%',
+    height: 64,
   },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
+  flatList: {
+    marginBottom: 10,
+    flex: 1,
   },
-  highlight: {
-    fontWeight: '700',
+  containerList: {
+    padding: 24,
+  },
+  containerImage: {
+    flexDirection: 'row',
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+    marginBottom: 30,
+  },
+  images: {
+    width: 50,
+    height: 50,
+    borderRadius: 25,
+    marginRight: 20,
   },
 });
 
